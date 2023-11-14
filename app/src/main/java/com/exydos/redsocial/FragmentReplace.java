fa88d88d0adb0b0255c2214d6bdbab6b23ad463d
edu <vr20i04002@usonsonate.edu.sv>
2023-11-13 22:14:28 -0600

    Parte1

---
+++ app/src/main/java/com/exydos/redsocial/FragmentReplace.java
@@ -0,0 +1,14 @@
+package com.exydos.redsocial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FragmentReplace extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment_replace);
+    }
+}
